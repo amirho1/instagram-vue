2bf0233b852763440b09fee2a90d322972275ea1
amirho1 <amirhosseinsalighedar@gmail.com>
2022-07-05 15:23:37 +0430

    write prelogin page

---
+++ src/components/logo/logo.vue
@@ -0,0 +1,13 @@
+<script setup>
+import styles from "./logo.module.scss";
+
+console.log(styles);
+</script>
+
+<template>
+  <div :class="[styles.wrapper]">
+    <img src="../../assets/logo.png" alt="instagram logo" />
+    <span> with </span>
+    <img src="../../assets/Vue.svg.png" alt="vue logo" width="50" height="50" />
+  </div>
+</template>
---
+++ src/pages/preLogin.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Logo from "../components/logo/logo.vue";
+import styles from "./styles/preLogin.module.scss";
+import { f7 } from "framework7-vue";
+
+const onCreateNewAccountClick = () => {
+  f7.view.main.router.navigate("/register");
+};
+</script>
+
+<template>
+  <f7-page>
+    <Logo :class="styles.logo" />
+
+    <f7-button @click="onCreateNewAccountClick" fill :class="styles.btn"
+      >Create New account</f7-button
+    >
+
+    <f7-link :class="styles.login" href="/login">Login</f7-link>
+  </f7-page>
+</template>
